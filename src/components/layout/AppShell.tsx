de79c0cc25aaaf55c330383a4cacd8c8196b3b44
[dyad] <teixeiraedson252@gmail.com>
2025-12-30 20:53:56 -0600

    [dyad] wrote 2 file(s)

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -7,14 +7,16 @@ import { AppHeader } from "@/components/AppHeader";
 
 const AppShell = () => {
   return (
-    <AppSidebarLayout>
-      <div className="flex min-w-0 flex-1 flex-col bg-background text-foreground">
-        <AppHeader />
-        <div className="flex-1 overflow-y-auto overflow-x-hidden">
-          <Outlet />
+    <div className="h-screen overflow-hidden">
+      <AppSidebarLayout>
+        <div className="flex min-w-0 flex-1 flex-col bg-background text-foreground">
+          <AppHeader />
+          <div className="flex-1 overflow-y-auto overflow-x-hidden">
+            <Outlet />
+          </div>
         </div>
-      </div>
-    </AppSidebarLayout>
+      </AppSidebarLayout>
+    </div>
   );
 };
 
